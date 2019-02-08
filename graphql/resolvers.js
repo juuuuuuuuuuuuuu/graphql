ef7615ae5ebffd87cd486d6de9a30d157d57f06c
이주영 <shm03088@douzone.com>
2019-02-08 17:02:15 +0900

    버그수정

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,8 +3,8 @@ import { getById, getMovies, addMovie, deleteMovie, getAPIMovies } from './db';
 const resolvers = {
     Query: {
         movies: () => getMovies(),
-        movie: (_, { id }) => getById(id)
-        APIMovies: (_, { rating, limit }) => getAPIMovies(limit, rating)
+        movie: (_, { id }) => getById(id),
+        apiMoive: (_, { rating, limit }) => getAPIMovies(limit, rating)
     },
     Mutation: {
         addMovie: (_, { name, score }) => addMovie(name, score),
